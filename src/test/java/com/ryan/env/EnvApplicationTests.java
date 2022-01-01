982fa8b725379973b75b67449a4a8eff5a99176d
ryan <ryan_zhang@trendmicro.com>
2022-01-01 14:53:19 +0800

    repo init

---
+++ src/test/java/com/ryan/env/EnvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ryan.env;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnvApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
